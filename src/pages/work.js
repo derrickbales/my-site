0c320bf079e4948c9a34564f0bafc12ba1665474
Derrick Bales <balesderrick@gmail.com>
2019-07-22 14:20:51 -0700

    stable

--- src/pages/work.js
+++ src/pages/work.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Work = () => (
   <div>
-    <h1>Work page</h1>
-    <p>this is my website.</p>
+    <h1>My Work</h1>
+    <p>under construction.</p>
   </div>
 );
